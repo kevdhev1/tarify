642ebbbd2ff5ce5b33d627070222560bcd623441
Kevin Marquez <kammper10@gmail.com>
2026-08-14 18:42:33 -0500

    feat: add header structure

---
+++ src/components/sections/Header/Header.tsx
@@ -0,0 +1,16 @@
+import CalculatorIcon from "@/icons/calculator";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.icon}>
+        <CalculatorIcon />
+      </div>
+      <h1 className={styles.title}>Tarify</h1>
+      <p className={styles.description}>
+        Calcula un precio sostenible para tus proyectos freelance en segundos
+      </p>
+    </header>
+  );
+}
